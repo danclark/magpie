d7bff87693812cac1fb38b2973a7dbbc1964e7e6
Daniel Clark <nonchill@outlook.com>
2013-06-10 20:52:03 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magpie::Application.config.secret_token = '2fce97455164ae5ee568acb1f9c2c94faf74e4894389819d5b6bc213b00f0e0318b9444861baf04c9b73116bbbda46e1af0c7977667f23eaffa15dceb3f706ec'
