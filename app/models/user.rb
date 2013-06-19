09459674c8c99d0cc9c91eb7f0773bc61526b511
Daniel Clark <nonchill@outlook.com>
2013-06-19 15:46:18 -0400

    Started duplicate test, issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :username
+  attr_accessible :email, :password, :username, :password_confirmation
+
+	validate :username, presence: true, uniqueness: true
+	validate :password, confirmation: true, presence: true
+	validate :email, presence: true, uniqueness: true
 end
