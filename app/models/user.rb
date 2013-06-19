18e6f50ba9a80bed031e2a6209163f0bfd93d29c
Daniel Clark <nonchill@outlook.com>
2013-06-19 19:32:18 -0400

    Fixed validation issues.  validate != validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :username, :password_confirmation
 
-	validate :username, presence: true, uniqueness: true
-	validate :password, confirmation: true, presence: true
-	validate :email, presence: true, uniqueness: true
+	validates :username, presence: true, uniqueness: true
+	validates :password, confirmation: true, presence: true
+	validates :email, presence: true, uniqueness: true
 end
