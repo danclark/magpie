83772dea569e935a78750001a963f1625450196d
Daniel Clark <nonchill@outlook.com>
2013-06-20 20:06:36 -0400

    Week 7 part 1 of HW complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
 
 	validates :username, presence: true, uniqueness: true
 	validates :password, confirmation: true, presence: true
-	validates :email, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]+\z/ }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,49 @@
-require "test_helper"
+require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe User do
+	describe 'validations' do
+		let(:user) { FactoryGirl.build :user }
+
+		it 'must have an email address' do
+			user.email = ''
+			user.valid?.wont_equal true
+			user.errors[:email].must_include "can't be blank"
+		end
+
+		it 'must have a unique email address' do
+			FactoryGirl.create :user
+			user.valid?.wont_equal true
+			user.errors[:email].must_include "has already been taken"
+		end
+
+		it 'must have a valid email address' do
+			user.email = 'dclark8@'
+			user.valid?.wont_equal true
+		end
+
+		it 'must have a username' do
+			user.username = ''
+			user.valid?.wont_equal true
+			user.errors[:username].must_include "can't be blank"
+		end
+
+		it 'must have a unique username' do
+			FactoryGirl.create :user
+			user.valid?.wont_equal true
+			user.errors[:username].must_include "has already been taken"
+		end
+
+		it 'must have a password' do
+			user.password = ''
+			user.valid?.wont_equal true
+			user.errors[:password].must_include "can't be blank"
+		end
+
+		it 'must have matching passwords' do
+			user.password_confirmation = 'abc1234'
+			user.valid?.wont_equal true
+			user.errors[:password].must_include "doesn't match confirmation"
+		end
+
+	end
 end
