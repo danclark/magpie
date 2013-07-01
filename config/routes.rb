e6e20985548e73af201883c1ff4e0c34b171f0ff
Daniel Clark <nonchill@outlook.com>
2013-07-01 15:03:38 -0400

    Added sessions create route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Magpie::Application.routes.draw do
 	match '/lists' => 'lists#index'
 	resources :users, only: [:show, :new, :create, :index]
 
+	resources :sessions, only: [:create]
 	match '/signin' => 'sessions#new'
 
 end
