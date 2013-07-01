4bc7bfb8fb73783a5b638d96f2d6482e632e1c07
Daniel Clark <nonchill@outlook.com>
2013-07-01 15:36:20 -0400

    Added sessions_controller.rb code for new and create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
 	def new
+		@users = User.new
 	end
 
 	def create
+		@user = login(params[:user][:email], params[:user][:password])
+		redirect_to root_path
 	end
 end
